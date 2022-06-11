003f9b57e943331cad9e288c88270a110d15e569
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-11 22:55:53 +0300

    feat:adds models

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = models.ImageField(upload_to = 'images/', blank=True)
+    bio = models.TextField(max_length=240, null=True)
+    
+    def __str__(self):
+        return self.user
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    pic = models.ImageField(upload_to = 'posts/', blank=False)
+    description = models.TextField(max_length=255)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return {self.title}
+    
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+    design = models.IntegerField(choices=rating, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return {self.user}
