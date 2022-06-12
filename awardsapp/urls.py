4242162346cf6bbd1dcbac3a305fe123f4562f3a
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-12 03:27:13 +0300

    feat:add profile form

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.index, name = 'indexpage'),
-    path('accounts/profile/',views.profile,name = 'profile')
+    path('accounts/profile/',views.profile,name = 'profile'),
+    path(r'update_profile', views.update_profile, name='update'),
 ]
