4a60d6f32ecb8c6a1f2ca65bdc3f2a2b95a25a44
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-16 02:04:16 +0300

    feat:adds tests

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import Post, Profile, Rating, User
+
 
 # Create your tests here.
+
+
+
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='chanai')
+        self.profile = Profile.objects.create(user = self.user,bio = 'simba')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_profile(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+  
+class PostTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='chanai')
+        self.profile = Profile.objects.create(user = self.user,bio = 'layoni')
+
+        self.project = Post.objects.create(user = self.user,profile = self.profile,title = 'Awardss',location='Nairobi',description='Avengers Assemble',link= 'chanai-gallery.herokuapp.com/',date='06/06/2022', technologies='Django')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Post))
+    
+    
+    def test_save_project(self):
+        self.project.save_post()
+        project = Post.objects.all()
+        self.assertTrue(len(project) > 0)
+    
+    def test_delete_project(self):
+        self.project.delete_post()
+        project = Post.search_by_posts('Awardss')
+        self.assertTrue(len(project) < 1)
+
+
+    def test_find_project(self):
+        self.project.save()
+        project = Post.search_by_posts('Awardss')
+        self.assertTrue(len(project) > 0)
+        
+
+ 
